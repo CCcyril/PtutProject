1150984c94ee7047f21f5fde5235ab4a6e9085e5
IUT <cally.cyril@hotmail.fr>
2015-04-01 23:54:19 +0200

    Update DataBase

---
+++ src/CGG/ConferenceBundle/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user01
+ * Date: 01/04/15
+ * Time: 23:14
+ */
+
+namespace CGG\ConferenceBundle\DataFixtures\ORM;
+
+
+class LoadUser {
+
+}
---
+++ src/CGG/ConferenceBundle/Entity/ConferenceUser.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user01
+ * Date: 01/04/15
+ * Time: 22:07
+ */
+
+namespace CGG\ConferenceBundle\Entity;
+
+
+class ConferenceUser {
+
+}
