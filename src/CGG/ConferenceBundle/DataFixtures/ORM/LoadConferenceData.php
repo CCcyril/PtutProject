846d678dbe9d509ecd40134695310eee11e931bc
IUT <cally.cyril@hotmail.fr>
2015-04-01 12:14:37 +0200

    Conference fixture

--- src/CGG/ConferenceBundle/DataFixtures/ORM/LoadConferenceData.php
+++ src/CGG/ConferenceBundle/DataFixtures/ORM/LoadConferenceData.php
@@ -5,7 +5,24 @@
  * Date: 01/04/15
  * Time: 12:11
  */
+namespace CGG\ConferenceBundle\DataFixtures\ORM;
 
-class LoadConferenceData {
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use CGG\ConferenceBundle\Entity\Conference;
 
+class LoadConference implements FixtureInterface {
+
+    public function load(ObjectManager $manager)
+    {
+        for($i = 0; $i <3; $i++){
+            $conference = new Conference();
+            $conference->setName("Conférence".$i);
+            $conference->setStartDate(\date('r'));
+            $conference->setEndDate("09/09/2020");
+            $manager->persist($conference);
+        }
+        $manager->flush();
+
+    }
 }
