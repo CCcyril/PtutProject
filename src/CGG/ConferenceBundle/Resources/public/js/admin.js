98392971f88740a10782fd85b47fd53ddc5b82b3
IUT <cally.cyril@hotmail.fr>
2015-04-02 23:13:46 +0200

    Début sous-menu + corrections bugs + admin.js

--- src/CGG/ConferenceBundle/Resources/public/js/admin.js
+++ src/CGG/ConferenceBundle/Resources/public/js/admin.js
@@ -64,4 +64,73 @@ $(document).ready(function(){
     $(".container-edit-content").mouseleave(function(){
         $(this).find('input').hide();
     });
+
+    $("#addSubItem").on('click', function(event){
+        var url = Routing.generate('cgg_conference_admin_add_sub_item');
+        Routing.generate('cgg_conference_admin_add_sub_item');
+        var idConference = $("#idConference").val();
+        var itemId = event.target.id;
+        var idParent = $("#"+itemId).attr('data-menuItemId');
+        alert(url + ", " + idConference + ", " + idParent);
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                'idConference': idConference,
+                'idParent': idParent
+            },
+            dataType: "html",
+            success: function(){
+                window.location.reload(true);
+            }
+        });
+    });
+
+    $("#saveChangeHeadband").on('click', function(){
+        var url = Routing.generate('cgg_conference_admin_saveChangeHeadband');
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                'idConference': $("#idConference").val(),
+                'idPage': $("#idPage").val(),
+                'headbandTitle': $("#headbandTitle").text(),
+                'headbandText': $("#headbandText").text()
+            },
+            dataType: "html",
+            success:function() {
+                window.location.reload();
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+    });
+
+    $("#btnAddPages").on('click', function(){
+        $("#addPages").removeClass('hidden');
+    });
+
+    $("#addInput").on('click', function(){
+        var lastDiv = $("#addPages form div:last");
+        lastDiv.after("<div class='form-group'>" + lastDiv.html() + "</div>");
+        if($("#removeInput").hasClass('disabled')){
+            $("#removeInput").removeClass('disabled');
+        }
+    })
+
+    $("#removeInput").on('click', function(){
+        var countInput = $("#addPages form div").length;
+        if(countInput === 3){
+            $("#removeInput").addClass('disabled');
+        }
+        $("#addPages form div:last").remove();
+    });
+
+    $("#btnUpdateBackground").on('click', function(){
+        $("#updateBackground").removeClass('hidden');
+    });
+
+    $('.demo-auto').colorpicker();
 });
